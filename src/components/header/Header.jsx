28397bc2e8afd89bf6d94f90e54b44a4c19b0cf7
asim-siddiqui02 <asim@adabdigital.com>
2026-08-12 00:59:06 +0530

    fixed mobile navbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -68,6 +68,10 @@ const Header = () => {
     return () => window.removeEventListener('asim-theme-change', handleExternalThemeChange);
   }, []);
 
+  useEffect(() => {
+    document.body.classList.toggle('nav-menu-open', toggle);
+  }, [toggle]);
+
   // Save the theme preference to local storage and apply it
   const handleThemeChange = (event) => {
     const newTheme = event.target.checked ? 'dark' : 'light';
@@ -83,12 +87,12 @@ const Header = () => {
 
         <div className={toggle ? "nav__menu show-menu" : "nav__menu"}>
           <ul className="nav__list">
-            <li className="nav__item"><a href="#home" className="nav__link active-link"><i className="uil uil-estate nav__icon"></i>Home</a></li>
-            <li className="nav__item"><a href="#about" className="nav__link"><i className="uil uil-user nav__icon"></i>About</a></li>
-            <li className="nav__item"><a href="#skills" className="nav__link"><i className="uil uil-file-alt nav__icon"></i>Skills</a></li>
-            <li className="nav__item"><a href="#services" className="nav__link"><i className="uil uil-briefcase-alt nav__icon"></i>Services</a></li>
-            <li className="nav__item"><a href="#portfolio" className="nav__link"><i className="uil uil-scenery nav__icon"></i>Portfolio</a></li>
-            <li className="nav__item"><a href="#contact" className="nav__link"><i className="uil uil-message nav__icon"></i>Contact</a></li>
+            <li className="nav__item"><a href="#home" className="nav__link active-link" onClick={() => setToggle(false)}><i className="uil uil-estate nav__icon"></i>Home</a></li>
+            <li className="nav__item"><a href="#about" className="nav__link" onClick={() => setToggle(false)}><i className="uil uil-user nav__icon"></i>About</a></li>
+            <li className="nav__item"><a href="#skills" className="nav__link" onClick={() => setToggle(false)}><i className="uil uil-file-alt nav__icon"></i>Skills</a></li>
+            <li className="nav__item"><a href="#services" className="nav__link" onClick={() => setToggle(false)}><i className="uil uil-briefcase-alt nav__icon"></i>Services</a></li>
+            <li className="nav__item"><a href="#portfolio" className="nav__link" onClick={() => setToggle(false)}><i className="uil uil-scenery nav__icon"></i>Portfolio</a></li>
+            <li className="nav__item"><a href="#contact" className="nav__link" onClick={() => setToggle(false)}><i className="uil uil-message nav__icon"></i>Contact</a></li>
           </ul>
 
           <i className="uil uil-times nav__close" onClick={() => setToggle(!toggle)}></i>
